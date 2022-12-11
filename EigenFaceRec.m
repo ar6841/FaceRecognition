1447debd292c076581ee8cefb42ea319ca5a21f4
Arjun Raja <ar6841@nyu.edu>
2022-12-11 17:32:36 -0500

    Updated code

--- EigenFaceRec.m
+++ EigenFaceRec.m
@@ -8,33 +8,37 @@ image_name = input('Enter image name, PLEASE ADD .jpg or .bmp: ','s');
 
 input_image = imread(append('Face dataset\Testing\',image_name));
 
-%% Start timer
-tic
+tic %Start timer
+
+% A constraint here is that all images are the same rows*cols size
+rows = size(input_image,1);
+cols = size(input_image,2);
 
 %% Read the images in training set
 TrainingDatastore = imageDatastore('Face dataset\Training\');
+
 M = length(TrainingDatastore.Files); % Number of images
-for i=1:M
+
+training_images = zeros(rows,cols,M); %Preallocate for efficiency
+for i = 1:M
     training_images(:,:,i) = imread(TrainingDatastore.Files{i,1});
 end
-rows = size(training_images,1);
-cols = size(training_images,2);
+
 N = rows*cols;
 %% Calculate column vector meanface m
 
 avg_matrix_m = sum(training_images,3)/M; % Add up all the NxN images and divide by number of images
 
 m =zeros(N,1); %Preallocate for efficiency
-for i=1:rows
-    m(cols*(i-1)+1:cols*i,1)=avg_matrix_m(i,:)'; %stacking the rows as coulmn vectors
+for i = 1:rows
+    m(cols*(i-1)+1:cols*i,1) = avg_matrix_m(i,:)'; %stacking the rows as coulmn vectors
 end
-
 
 %% Create column vector of training images R
 R = zeros(N, M); %Preallocate memory for efficiency
 for i = 1:M
     for j=1:rows
-        R(cols*(j-1)+1:cols*j,i)=training_images(j,:,i)'; %stacking the rows for each image as a column vectors together in one matrix
+        R(cols*(j-1)+1:cols*j,i) = training_images(j,:,i)'; %stacking the rows for each image as a column vectors together in one matrix
     end 
 end
 
@@ -58,8 +62,8 @@ omega_full = U'*A;
 
 %% Reshape input image
 I =zeros(N,1); %Preallocate for efficiency
-for i=1:rows
-    I(cols*(i-1)+1:cols*i,1)=input_image(i,:)'; %stacking the rows as coulmn vector
+for i = 1:rows
+    I(cols*(i-1)+1:cols*i,1) = input_image(i,:)'; %stacking the rows as coulmn vector
 end
 
 I = I-m; %Shift of center for input image
@@ -72,7 +76,7 @@ omega_I = U'*I;
 distances = zeros(1,M); %Preallocate for efficiency
 least_dist = inf;
 least_dist_index = 0;
-for i = 1:M
+for i = 1:M %Loop to find the nearest distance and its index
     distances(i) = euclid_distance(omega_I,omega_full(:,i));
     if(distances(i) < least_dist)
         least_dist = distances(i);
@@ -85,14 +89,16 @@ imwrite(avg_matrix_m, "Face dataset\MeanFace.jpg");
 
 %% Save the eigen faces
 eignFace = zeros(rows,cols); %Prealloca for efficiency 
-for i=1:M
+for i = 1:M
     for j = 1:rows
         eignFace(j,:) = U(cols*(j-1)+1:cols*j,i); %reshape column vector to rows*cols
     end
     eignFace = uint8(round(eignFace));
     imwrite(eignFace,"Face dataset\EigenFaces\"+ string(i)+ ".jpg");
 end
-toc
+
+toc % Stop timer
+
 %% Open the least distance training image and input image (These are the outputs)
 f1 = figure;
 imshow(input_image)
@@ -116,9 +122,9 @@ function dist = euclid_distance(x1,x2)
         error('Vecor dimensions are not compatible');
     else
 
-        v = (x1-x2).^2;
-        total = sum(v);
-        dist = sqrt(total);
+        v = (x1-x2).^2; %Element wise square
+        total = sum(v); % Add up all the elements
+        dist = sqrt(total); % square root
     end
 
 end
